eafe99a815408dd6aa62438e5e1097618a598380
Cris Shaki <cmshaki@gmail.com>
2019-10-28 14:10:31 +0300

    Small error on setting model

--- src/models/setting.model.ts
+++ src/models/setting.model.ts
@@ -5,9 +5,9 @@ export class Setting extends Entity {
   @property({
     type: 'number',
     id: true,
-    generated: true,
+    required: true,
   })
-  id?: number;
+  id: number;
 
   @property({
     type: 'string',
