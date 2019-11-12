64eb70fab440ab7b87a0ca86c7ab84cdf6d667af
Cris Shaki <cmshaki@gmail.com>
2019-11-12 13:03:56 +0300

    Refined Order Emails

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -5,7 +5,6 @@ import {Order} from '../models';
 import {OrderRepository, SettingRepository} from '../repositories';
 import {AuthenticationBindings, authenticate} from '@loopback/authentication';
 import {UserProfile} from '@loopback/security';
-import * as nodemailer from 'nodemailer';
 import * as emailtemplate from 'email-templates';
 
 export class PaymentController {
@@ -36,21 +35,27 @@ export class PaymentController {
     const emailSetting: any = await this.settingRepository.find({
       where: {key: 'email'},
     });
-    const toAddresses = await (emailSetting[0].value.to || '').split(',');
+    const bccAddresses = await (emailSetting[0].value.bcc || '').split(',');
     const email = new emailtemplate({
       message: {
-        from: emailSetting[0].value.from,
+        from: `"Bestellungen Schneckenhof" <${emailSetting[0].value.from}>`,
       },
+      send: true,
       transport: {
-        jsonTransport: true,
+        host: 'smtp.strato.de',
+        port: 465,
+        auth: {
+          user: 'bestellungen@weingut-schneckenhof.de',
+          pass: 'Bestellungen@@@@@@',
+        },
       },
-      preview: true,
+      preview: false,
     });
     if (payOrder.items) {
-      return await email
+      await email
         .send({
-          template: 'invoice',
-          message: {to: toAddresses},
+          template: 'invoice-admin',
+          message: {to: emailSetting[0].value.to, bcc: bccAddresses},
           locals: {
             realname: payOrder.realname,
             items: payOrder.items,
@@ -62,33 +67,27 @@ export class PaymentController {
             message: payOrder.message,
           },
         })
-        .then(async (emailtemplate: any) => {
-          const transporter = nodemailer.createTransport({
-            host: 'smtp.strato.de',
-            port: 465,
-            auth: {
-              user: 'bestellungen@weingut-schneckenhof.de',
-              pass: 'Bestellungen@@@@@@',
-            },
-          });
+        .then(async () => {
+          console.log('New Order Email has been sent to Admin');
+        });
 
-          const templates = JSON.parse(emailtemplate.message);
-          const message = {
-            from: `"Bestellungen Schneckenhof" <${emailSetting[0].value.from}>`,
-            to: emailSetting[0].value.to,
-            bcc: emailSetting[0].value.bcc,
-            subject: '✨Neue Bestellung',
-            text: templates.text,
-            html: templates.html,
-          };
-
-          const sendMessage = await transporter.sendMail(message);
-
-          console.log(
-            'New Order Email has been sent of messageId: %s',
-            sendMessage.messageId,
-          );
-
+      return email
+        .send({
+          template: 'invoice',
+          message: {to: payOrder.email},
+          locals: {
+            realname: payOrder.realname,
+            items: payOrder.items,
+            email: payOrder.email,
+            street: payOrder.street,
+            zip: payOrder.zip,
+            city: payOrder.city,
+            phone: payOrder.phone,
+            message: payOrder.message,
+          },
+        })
+        .then(async () => {
+          console.log('New Order Email has been sent to Client');
           await this.orderRepository.create({
             id: Math.floor(1000 + Math.random() * 900000),
             type: 'email',
